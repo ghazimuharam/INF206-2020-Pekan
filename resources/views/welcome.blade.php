ccde7b2653559db04b12595d7f1b3908f398b89d
T.M. Syahril Nur A <syahrilnur@mhs.unsyiah.ac.id>
2020-05-11 21:44:10 +0700

    add market options

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #f8f9fa;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
