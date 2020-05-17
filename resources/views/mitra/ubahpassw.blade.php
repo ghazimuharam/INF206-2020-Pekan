62979c3ef643ffca54925e964ae3d6c9d19ae247
almatunira <almatunira@mhs.unsyiah.ac.id>
2020-05-17 21:25:59 +0700

    ubahpassw

---
+++ resources/views/mitra/ubahpassw.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.main')
+
+@section('title', 'Ubah Password');
+
+@section('content')
+<div class="container">
+	<div class="row justify-content-center mt-5">
+		<div class="col-md-5">
+			<p class="h1 text-center mb-4"><i class="fa fas-car"></i> Ubah Password</p>
+				@if(session('error'))
+					<div class="alert alert-danger text-center" >
+						{{session('error')}}
+					</div>
+				@endif
+			  <form action="{{url('/user/password/'.session('id'))}}" method="post" enctype="multipart/form-data">
+					@csrf
+					{{method_field('PATCH')}}
+				<div class="form-group mb-2">
+					<p class="mb-1">Password saat ini</p>
+					<input type="password" class="form-control" name="password_sekarang" required>
+				</div>
+				<div class="form-group mb-2">
+					<p class="mb-1">Password baru</p>
+					<input type="password" class="form-control" name="password_baru" required>
+				</div>
+				<div class="form-group mb-3">
+					<p class="mb-1">Konfirmasi Password baru</p>
+					<input type="password" class="form-control" name="konfirmasi_password_baru" required>
+				</div>
+
+		      	<button type="submit" class="btn btn-primary">Ubah Password</button>
+		      	 <a href="{{url()->previous()}}" class="btn btn-danger">Batal</a>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
