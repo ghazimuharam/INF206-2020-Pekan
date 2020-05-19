a8752ff8a4030beabd6a909f41054212c36f8936
Farah Anisa <farah.ab@mhs.unsyiah.ac.id>
2020-05-19 21:28:14 +0700

    edit Pilih Pasar Pembeli

--- resources/views/pembeli/opsiPasar.blade.php
+++ resources/views/pembeli/opsiPasar.blade.php
@@ -7,11 +7,11 @@
     <h3 class="mt-3">Pilih Pasar</h3>
 </div>
 <div class="row justify-content-center mt-2">
-    <div class="col-sm-3 text-center">
-        <a class="btn btn-secondary btn-lg" href="/buyer/marketOrder">Pasar Ikan</a>
+    <div class="col-sm-3 text-center my-1">
+        <a class="btn btn-secondary btn-lg btn-block" href="/buyer/marketOrder"><img src="https://marvel-live.freetls.fastly.net/canvas/2020/3/5d5124973e2c44a89c91d7765d92e6d1?quality=95&fake=.png" heigth="30" width="30"> Pasar Ikan</a>
     </div>
-    <div class="col-sm-3 text-center">
-        <a class="btn btn-secondary btn-lg" href="{{ route('pembeliorders') }}">Pasar Sayur</a>
+    <div class="col-sm-3 text-center my-1">
+        <a class="btn btn-secondary btn-lg btn-block" href="{{ route('pembeliorders') }}"><img src="https://marvel-live.freetls.fastly.net/canvas/2020/3/352ca90e7b534cac8a146b1c10aadfa3?quality=95&fake=.png" heigth="30" width="30"> Pasar Sayur</a>
     </div>
 </div>
 @endsection
